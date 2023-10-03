2c814297c1b35e6402f68aee56e44fa36a4bdbda
Carl Willman <carl@willman.nu>
2023-10-03 11:17:52 +0200

    splits tests into seperate file

--- simplex_solver.py
+++ simplex_solver.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 
 import argparse
-import sys
-import unittest
-from typing import Any
 
 import numpy as np
 from scipy.optimize import linprog
-
-
-# Parse Matrix Ax = b of size (n+1) x m, Vector c of size n.
 def parse_command_line_args():
     parser = argparse.ArgumentParser(
         description="Read matrix A, vectors b and c from the command line.")
@@ -23,8 +17,6 @@ def parse_command_line_args():
     parser.add_argument("--checker", help="Check output with scipy",
                         action="store_true", required=False)
     parser.add_argument("-v", "--verbose", help="print steps",
-                        action="store_true", required=False)
-    parser.add_argument("--test", help="Run unit tests",
                         action="store_true", required=False)
     args = parser.parse_args()
     return args
@@ -153,99 +145,8 @@ def is_basic_variable(T, j):
     return (True, row_i_one) if (num_zeros == len(T) - 1) and (num_ones == 1) else (False, None)
 
 
-class TestLP(unittest.TestCase):
-    def assertLP(self, A, b, c):
-        print(
-            '\n---------------------------------------------------------------------------')
-        result = solve(A, b, c, verbose=True)
-        expected = linprog(-c, A_ub=A, b_ub=b)
-        self.assertEqual(result[1], -expected.fun)
-        print(result)
-        print(list(expected.x), -expected.fun)
-        self.assertEqual(np.sum(c * result[0]), result[1])
-        self.assertEqual(np.sum(c * result[0]), -expected.fun)
-
-    def test_sanity(self):
-        A = np.array([[4, 2], [2, 3]])
-        b = np.array([32, 24])
-        c = np.array([5, 4])
-        self.assertLP(A, b, c)
-
-    def test_sanity_1(self):
-        A = np.array([[1, 1], [1, 2], [4, 3]])
-        b = np.array([10, 15, 38])
-        c = np.array([750, 1000])
-        self.assertLP(A, b, c)
-
-    def test_multiple_sol(self):
-        A = np.array([[1, 1, 0], [0, 1, 3], [2, 5, 9]])
-        b = np.array([2, 3, 10])
-        c = np.array([1, 2, 3])
-        self.assertLP(A, b, c)
-
-    def test_no_init_bfs(self):
-        A = np.array([[-1, -2], [-1, 2], [-1, 2]])
-        b = np.array([-3, -1, 2])
-        c = np.array([1, 3])
-        res = solve(A, b, c, verbose=True)
-        self.assertEqual(res[0], None)
-        self.assertEqual(res[1], None)
-
-    # def test_unbounded_solution(self):
-    #     A = np.array([[1, 1], [-1, 0], [0, -1]])
-    #     b = np.array([2, 0, 0])
-    #     c = np.array([1, 1])
-    #     res = solve(A, b, c, verbose=True)
-    #     self.assertEqual(res[0], None)  # No bounded solution for maximization
-    #     self.assertEqual(res[1], None)  # Optimal value should be infinite
-
-    def test_single_constraint(self):
-        A = np.array([[1, 2]])
-        b = np.array([4])
-        c = np.array([2, 1])
-        self.assertLP(A, b, c)
-
-    # def test_infeasible(self):
-    #     A = np.array([[1, 1], [-1, -1], [1, -1]])
-    #     b = np.array([1, -2, 0])
-    #     c = np.array([1, 1])
-    #     res = solve(A, b, c, verbose=True)
-    #     self.assertEqual(res[0], None)
-    #     self.assertEqual(res[1], None)
-
-    def test_trivial(self):
-        A = np.array([[1, 0], [0, 1]])
-        b = np.array([1, 1])
-        c = np.array([1, 1])
-        self.assertLP(A, b, c)
-
-    def test_tight_constraints(self):
-        A = np.array([[1, 1], [1, -1]])
-        b = np.array([1, 0])
-        c = np.array([1, 1])
-        self.assertLP(A, b, c)
-
-    def test_zero_objective(self):
-        A = np.array([[1, 2], [2, 3]])
-        b = np.array([4, 7])
-        c = np.array([0, 0])
-        self.assertLP(A, b, c)
-
-    # def test_negative_coefficients(self):
-    #     A = np.array([[-1, 2], [2, -3]])
-    #     b = np.array([-1, 4])
-    #     c = np.array([-1, -2])
-    #     self.assertLP(A, b, c)
-
-
 if __name__ == "__main__":
     np.set_printoptions(precision=3, suppress=True)
-
-    if len(sys.argv) > 1 and sys.argv[1] == '--test':
-        del sys.argv[1]
-        unittest.main()
-        exit()
-
     args = parse_command_line_args()
     A = np.array([list(map(float, row.split()))
                   for row in args.matrix_A.strip().split(';')])
---
+++ test_lp.py
@@ -0,0 +1,93 @@
+import unittest
+import numpy as np
+from simplex_solver import solve
+from scipy.optimize import linprog
+
+class TestLP(unittest.TestCase):
+    def assertLP(self, A, b, c):
+        print(
+            '\n---------------------------------------------------------------------------')
+        result = solve(A, b, c, verbose=True)
+        expected = linprog(-c, A_ub=A, b_ub=b)
+        self.assertEqual(result[1], -expected.fun)
+        print(result)
+        print(list(expected.x), -expected.fun)
+        self.assertEqual(np.sum(c * result[0]), result[1])
+        self.assertEqual(np.sum(c * result[0]), -expected.fun)
+
+    def test_sanity(self):
+        A = np.array([[4, 2], [2, 3]])
+        b = np.array([32, 24])
+        c = np.array([5, 4])
+        self.assertLP(A, b, c)
+
+    def test_sanity_1(self):
+        A = np.array([[1, 1], [1, 2], [4, 3]])
+        b = np.array([10, 15, 38])
+        c = np.array([750, 1000])
+        self.assertLP(A, b, c)
+
+    def test_multiple_sol(self):
+        A = np.array([[1, 1, 0], [0, 1, 3], [2, 5, 9]])
+        b = np.array([2, 3, 10])
+        c = np.array([1, 2, 3])
+        self.assertLP(A, b, c)
+
+    def test_no_init_bfs(self):
+        A = np.array([[-1, -2], [-1, 2], [-1, 2]])
+        b = np.array([-3, -1, 2])
+        c = np.array([1, 3])
+        res = solve(A, b, c, verbose=True)
+        self.assertEqual(res[0], None)
+        self.assertEqual(res[1], None)
+
+    # def test_unbounded_solution(self):
+    #     A = np.array([[1, 1], [-1, 0], [0, -1]])
+    #     b = np.array([2, 0, 0])
+    #     c = np.array([1, 1])
+    #     res = solve(A, b, c, verbose=True)
+    #     self.assertEqual(res[0], None)  # No bounded solution for maximization
+    #     self.assertEqual(res[1], None)  # Optimal value should be infinite
+
+    def test_single_constraint(self):
+        A = np.array([[1, 2]])
+        b = np.array([4])
+        c = np.array([2, 1])
+        self.assertLP(A, b, c)
+
+    # def test_infeasible(self):
+    #     A = np.array([[1, 1], [-1, -1], [1, -1]])
+    #     b = np.array([1, -2, 0])
+    #     c = np.array([1, 1])
+    #     res = solve(A, b, c, verbose=True)
+    #     self.assertEqual(res[0], None)
+    #     self.assertEqual(res[1], None)
+
+    def test_trivial(self):
+        A = np.array([[1, 0], [0, 1]])
+        b = np.array([1, 1])
+        c = np.array([1, 1])
+        self.assertLP(A, b, c)
+
+    def test_tight_constraints(self):
+        A = np.array([[1, 1], [1, -1]])
+        b = np.array([1, 0])
+        c = np.array([1, 1])
+        self.assertLP(A, b, c)
+
+    def test_zero_objective(self):
+        A = np.array([[1, 2], [2, 3]])
+        b = np.array([4, 7])
+        c = np.array([0, 0])
+        self.assertLP(A, b, c)
+
+    # def test_negative_coefficients(self):
+    #     A = np.array([[-1, 2], [2, -3]])
+    #     b = np.array([-1, 4])
+    #     c = np.array([-1, -2])
+    #     self.assertLP(A, b, c)
+
+
+
+def run_tests():
+    unittest.main()
